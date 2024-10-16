6042a1ca5d2100a96012192024249ac626f0a5ef
Code-Cruncher-Omega <earella6@gmail.com>
2024-10-16 19:54:34 -0400

    Update Validator.java

--- src/main/java/org/example/Validator.java
+++ src/main/java/org/example/Validator.java
@@ -4,7 +4,27 @@ public class Validator {
 
     private Validator() {}
 
-    public String parseString(String text) {
-        return "";
+    private double[] parseStringToDoubleArray(String text) throws Exception {
+        boolean comma = text.contains(",");
+        boolean newLine = text.contains("\n");
+
+        String[] snippets;
+
+        if(comma && !newLine) {
+            snippets = text.split(",");
+        } else if(!comma && newLine) {
+            snippets = text.split("\n");
+        } else if(!comma) {
+            snippets = new String[0];
+        } else
+            throw new Exception("Invalid input format");
+
+        double[] values = new double[snippets.length];
+        for(int i = 0 ; i < snippets.length ; i++) {
+            values[i] = Double.parseDouble(snippets[i]);
+        }
+        return values;
     }
+
+    public String 
 }
