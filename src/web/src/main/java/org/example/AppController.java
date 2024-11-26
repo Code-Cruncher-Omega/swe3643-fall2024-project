d2c855e41b0d941dbc5360a94e146d11e6b320c6
Code-Cruncher-Omega <earella6@gmail.com>
2024-11-26 03:31:28 -0500

    Switch to Thymeleaf instead of jsp

--- src/web/src/main/java/org/example/AppController.java
+++ src/web/src/main/java/org/example/AppController.java
@@ -2,22 +2,29 @@ package org.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
 
 @Controller
 public class AppController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+    @RequestMapping("/")
+    public String output(@RequestParam(name="output", required=false, defaultValue = "Enter values below, then select an operation") String output,
+            @RequestParam(name="values", required=false, defaultValue = "") String values,
+            @RequestParam(name="isError", required=false, defaultValue="false") boolean isError,
+            @RequestParam(name="method", required=false, defaultValue="0") String operation, Model model) {
 
-    @GetMapping("/output")
-    public String output(@RequestParam(name="output", required=false, defaultValue="Enter values below, then select an operation") String output, Model model) {
-
+        if(!values.isEmpty()) {
+            isError = false;
+        }   else {
+            isError = true;
+            output = "big fat failiure";
+        }
+        model.addAttribute("isError", isError);
         model.addAttribute("output", output);
+        model.addAttribute("method", operation);
         return "index";
     }
 
--- src/web/src/main/java/org/example/Application.java
+++ src/web/src/main/java/org/example/Application.java
@@ -1,13 +1,15 @@
 package org.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
-
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(Application.class)
+				.properties("spring.config.location=src/web/src/main/resources/application.properties")
+				.run(args);
 	}
 
 }
