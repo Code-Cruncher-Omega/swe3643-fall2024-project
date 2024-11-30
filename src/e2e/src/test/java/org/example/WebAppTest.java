289c7bfed1c471b866ce683d552cb927be312474
Code-Cruncher-Omega <earella6@gmail.com>
2024-11-30 00:59:47 -0500

    Updated e2e tests for better readability

--- src/e2e/src/test/java/org/example/WebAppTest.java
+++ src/e2e/src/test/java/org/example/WebAppTest.java
@@ -16,6 +16,7 @@ public class WebAppTest {
     void calculatorWebUi_pageTitle_isCalculator(Page page) {
         //preq-E2E-TEST-5
         page.navigate("http://localhost:8080");
+
         assertThat(page.getByTitle("Calculator"));
     }
 
@@ -23,9 +24,11 @@ public class WebAppTest {
     void calculatorWebUi_computeSampleStandardDeviationButton_outputsResult(Page page) {
         //preq-E2E-TEST-6
         page.navigate("http://localhost:8080");
+
         page.getByLabel("").click();
         page.getByLabel("").fill("9\n2\n5\n4\n12\n7\n8\n11\n9\n3\n7\n4\n12\n5\n4\n10\n9\n6\n9\n4");
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Compute Sample Standard")).click();
+
         assertThat(page.locator("h3")).containsText("3.060787652326");
 
     }
@@ -34,7 +37,9 @@ public class WebAppTest {
     void calculatorWebUi_emptyInputPopulationStandardDeviationButton_outputsErrorMessage(Page page) {
         //preq-E2E-TEST-7
         page.navigate("http://localhost:8080");
+
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Compute Population Standard")).click();
+
         assertThat(page.locator("h3")).containsText("Invalid input");
 
     }
@@ -43,7 +48,9 @@ public class WebAppTest {
     void calculatorWebUi_emptyInputSampleStandardDeviationButton_outputsErrorMessage(Page page) {
         //preq-E2E-TEST-8
         page.navigate("http://localhost:8080");
+
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Compute Sample Standard")).click();
+
         assertThat(page.locator("h3")).containsText("Invalid input");
 
     }
@@ -52,9 +59,11 @@ public class WebAppTest {
     void calculatorWebUi_computeMeanButton_outputsResult(Page page) {
         //preq-E2E-TEST-9
         page.navigate("http://localhost:8080");
+
         page.getByLabel("").click();
         page.getByLabel("").fill("9\n2\n5\n4\n12\n7\n8\n11\n9\n3\n7\n4\n12\n5\n4\n10\n9\n6\n9\n4");
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Compute Mean")).click();
+
         assertThat(page.locator("h3")).containsText("7");
     }
 
@@ -62,9 +71,11 @@ public class WebAppTest {
     void calculatorWebUi_computeZScoreButton_outputsResult(Page page) {
         //preq-E2E-TEST-10
         page.navigate("http://localhost:8080");
+
         page.getByLabel("").click();
         page.getByLabel("").fill("5.5,7,3.060787652326");
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Compute Z Score")).click();
+
         assertThat(page.locator("h3")).containsText("-0.490069");
         // expected value changed from -0.49007 to -0.490069 since logic doesn't round up, plus this is more accurate
 
@@ -74,9 +85,11 @@ public class WebAppTest {
     void calculatorWebUi_computeSingleLinearRegressionFormula_outputResult(Page page) {
         //preq-E2E-TEST-11
         page.navigate("http://localhost:8080");
+
         page.getByLabel("").click();
         page.getByLabel("").fill("5,3\n3,2\n2,15\n1,12.3\n7.5,-3\n4,5\n3,17\n4,3\n6.42,4\n34,5\n12,17\n3,-1");
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Compute Single Linear Regression Formula")).click();
+
         assertThat(page.locator("h3")).containsText("y = -0.04596153293x + 6.933587781374");
         // Expected value again adjusted to account for the higher accuracy of the calculator, new expected value was
         // checked using multiple calculators to ensure accurate computations.
@@ -86,9 +99,11 @@ public class WebAppTest {
     void calculatorWebUi_computeYFromLinearRegressionFormula_outputResult(Page page) {
         //preq-E2E-TEST-12
         page.navigate("http://localhost:8080");
+
         page.getByLabel("").click();
         page.getByLabel("").fill("6,-0.04596,6.9336");
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Compute Y from Linear Regression Formula")).click();
+
         assertThat(page.locator("h3")).containsText("6.65784");
     }
 }
